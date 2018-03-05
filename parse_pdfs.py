1ccfd127a42b3dcb2ea1f0456ce18c22fa97d5aa
Roman Eisner <ithmatic@gmail.com>
2018-03-04 20:39:32 -0700

    working parsing!

--- parse_pdfs.py
+++ parse_pdfs.py
@@ -1,5 +1,5 @@
 from googlesearch import search # pip install google
-import PyPDF2
+import textract
 import requests
 import wget
 
@@ -10,30 +10,15 @@ google_search_query = 'filetype:pdf ' + google_search_query
 limit = 1
 count = 0
 tempfile = "temp.pdf"
-# for file in search(google_search_query):#, tld="co.in", num=10, stop=1, pause=2):
-#     count += 1
-#     if count > limit: break
-#     print(file)
-#     print("\n\n\n\n")
-#
-#     wget.download(file, tempfile)
-#
-#
-#     break
+for file_url in search(google_search_query):#, tld="co.in", num=10, stop=1, pause=2):
+    count += 1
+    if count > limit: break
+    print(file_url)
+    print("\n\n")
 
+    wget.download(file_url, tempfile)
 
-pdf_file = open(tempfile, 'rb')
-reader = PyPDF2.PdfFileReader(pdf_file)
-text = ""
-for page in range(1, reader.numPages + 1):
-    page = reader.getPage(count)
-    text += page.extractText()
+    text = textract.process(tempfile) #method='tesseract', language='eng')
+    print(text)
 
-print(text)
-print(text == '')
-
-# if text != "":
-#    text = text
-# #If the above returns as False, we run the OCR library textract to #convert scanned/image based PDF files into text
-# else:
-#    text = textract.process(fileurl, method='tesseract', language='eng')
+    break
