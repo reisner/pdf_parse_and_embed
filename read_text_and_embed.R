acfaa6977d7d98189daf2d7ec61c47c290d47e6c
Roman Eisner <roman.eisner@edmonton.ca>
2019-03-05 14:12:04 -0700

    Parse and make embeddings.

---
+++ read_text_and_embed.R
@@ -0,0 +1,35 @@
+library(coeNLP)
+
+text_file_dir = 'output/'
+
+all_text = c()
+files = list.files(text_file_dir)
+cat("Reading", length(files), "Files...")
+for (file in files) {
+  filepath = paste0(text_file_dir, file)
+  text_data = readLines(con = filepath, warn = FALSE)
+
+  short = nchar(text_data) < 5
+  # require(stringi)
+  # df[stri_length(df)>5]
+
+  num_words = unlist(purrr::map(text_data, function(str) stringi::stri_stats_latex(str)[['Words']]))
+
+  # Remove short lines:
+  remove_ind = which(short | (num_words < 2))
+
+  if (length(remove_ind) > 0) {
+    text_data = text_data[-remove_ind]
+  }
+
+  if (length(text_data) > 0) {
+    all_text = append(all_text, text_data)
+  }
+}
+
+cat("Done. Found", length(all_text), "Sentences.\n")
+browser()
+
+cat("Training Embedding...")
+model = coeNLP::trainEmbeddings(all_text, vectors = 100, lemmatize = TRUE)
+cat("Done.\n")
